1ac518ade57e91604ed37aaab515b74fccab11b9
vilisovdaniillite-oss <vilisovdaniillite@gmail.com>
2026-06-21 11:47:15 +0500

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Creator: sexyswag alone",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 4,
       },
     ],
   },
